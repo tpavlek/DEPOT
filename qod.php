06254b0040930a9fa5020b0d6ddc8b433bd6350a
Troy Pavlek <ebonwumon@depotwarehouse.net>
2011-10-28 06:49:52 -0600

    automated qod

--- qod.php
+++ qod.php
@@ -1,13 +1,7 @@
 <?php
-/*if(!isset($_COOKIE['block'])) {
-	header('Location: index.php?page=login');
-}
-require 'common.php';
-$query = "SELECT tid from topics where block = ";
-$query = $query . $_COOKIE['block'] . " AND author = 'QoD Block " . $_COOKIE['block'] . "'";
-$query = $query . " ORDER by date DESC";
-echo $query;
-$topics = $pdo->query($query);
-$arr = $topics->fetch();*/
-header('Location: index.php?page=viewTopic&tid=2506');
+require_once('obj/db.php');
+$db = DB::getInstance();
+$topic = $db->getLastTopic(3);
+$tid = $topic['data']['tid'];
+header('Location: index.php?page=viewTopic&tid=' . $tid );
 ?>
