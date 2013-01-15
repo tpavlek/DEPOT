6cc6300d06fd10db0dabf147579084067378a766
Troy Pavlek <ebonwumon@depotwarehouse.net>
2013-01-15 05:30:48 +0100

    removed old pages

--- pages/newTopic.php
+++
@@ -1,8 +0,0 @@
-<ul>
-<form action="?page=postaction&newtopic=1" method="POST">
-<p><li><label for="subject"> Subject: 
-<input class="reply" type="text" name="subject"></li></p>
-<p><li><label for="message"> Message:
-<textarea class="reply" name="message"></textarea></li></p>
-<input type="submit" value="GO GO!">
-</form>
--- pages/usercontrol.php
+++
@@ -1,7 +0,0 @@
-<span class="big">Log Out</span>
-<form action="?page=useraction&logout=1" method="POST">
-<input type="submit" value="Log Out">
-</form>
-
-<br><br>
-
